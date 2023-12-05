9bad7c7e8bed830d03d5e7be6309bb422c66d231
Jhonson <hbiblia@gmail.com>
2023-12-05 14:58:52 -0400

    angle mouse entity selected ctrl press

---
+++ Gobu/build.sh
@@ -0,0 +1,5 @@
+cd build
+ninja && meson install
+cd ..
+cd bin
+./gobu-editor
--- Gobu/engine/gobue/gobu_gizmos.c
+++ Gobu/engine/gobue/gobu_gizmos.c
@@ -4,6 +4,7 @@
 #include "gobu_input.h"
 #include "gobu_camera.h"
 #include "gobu_transform.h"
+#include "raygo/rlgl.h"
 #include "raygo/raymath.h"
 
 ECS_COMPONENT_DECLARE(ecs_gizmos_t);
@@ -11,6 +12,14 @@ ECS_COMPONENT_DECLARE(ecs_gizmos_t);
 static void GobuGizmos_Update(ecs_iter_t* it);
 static void GobuGizmos_Draw(ecs_iter_t* it);
 
+/**
+ * @brief Deselecciona todos los elementos.
+ *
+ * Esta función se utiliza para deseleccionar todos los elementos en el contexto de gizmos.
+ *
+ * @param it Puntero al iterador de entidades.
+ * @param gizmos Puntero a la estructura de gizmos.
+ */
 static void unselected_all(ecs_iter_t* it, ecs_gizmos_t* gizmos)
 {
     for (int i = it->count - 1; i >= 0; i--)
@@ -19,6 +28,13 @@ static void unselected_all(ecs_iter_t* it, ecs_gizmos_t* gizmos)
     }
 }
 
+/**
+ * Calcula el ángulo entre un punto y un rectángulo.
+ *
+ * @param mouse El punto del mouse.
+ * @param bonding El rectángulo de referencia.
+ * @return El ángulo entre el punto y el rectángulo.
+ */
 static double point_to_angle(Vector2 mouse, Rectangle bonding)
 {
     double p = sqrt(pow(bonding.x - mouse.x, 2) + pow(bonding.y - mouse.y, 2));
@@ -55,7 +71,7 @@ void GobuGizmosImport(ecs_world_t* world)
         }
     });
 
-    ECS_SYSTEM(world, GobuGizmos_Draw, phases->Draw, ecs_gizmos_t, GBoundingBox);
+    ECS_SYSTEM(world, GobuGizmos_Draw, phases->Draw, ecs_gizmos_t, GBoundingBox, GPosition, GScale, GRotation);
     ECS_SYSTEM(world, GobuGizmos_Update, EcsPostUpdate, ecs_gizmos_t, GBoundingBox, GPosition, GScale, GRotation);
 }
 
@@ -79,7 +95,7 @@ static void GobuGizmos_Update(ecs_iter_t* it)
     bool mouse_btn_pres_left = input->mouse.button_pressed(MOUSE_BUTTON_LEFT);
     bool mouse_btn_down_left = input->mouse.button_down(MOUSE_BUTTON_LEFT);
 
-    for (int i = it->count - 1; i > 0; i--)
+    for (int i = it->count - 1; i >= 0; i--)
     {
         Rectangle bonding = (Rectangle){ box[i].min.x, box[i].min.y, box[i].max.x, box[i].max.y };
         ecs_entity_t entity = it->entities[i];
@@ -107,11 +123,12 @@ static void GobuGizmos_Update(ecs_iter_t* it)
             {
                 // !TODO: Calcular el mouse con la camara para que se mueva correctamente, cuando tenemos un zoom diferente a 1.0f
                 // Rotamos la entidad seleccionada si tenemos Ctrl presionado
-                // pero si no lo tenemos presionado, movemos la entidad
+                // pero si no lo tenemos presionado, movemos la entidad.
                 if (ctrl) {
                     rot[i].x = point_to_angle(mouse, bonding);
                 }
                 else {
+                // movemos la entidad seleccionada
                     post[i].x += delta.x;
                     post[i].y += delta.y;
                 }
@@ -119,6 +136,13 @@ static void GobuGizmos_Update(ecs_iter_t* it)
         }
 
         // Duplicamos la entidad seleccionadas
+        if (ctrl && input->keyboard.pressed(KEY_D))
+        {
+            if (gizmos[i].selected)
+            {
+                ecs_clone(it->world, 0, entity, true);
+            }
+        }
     }
 }
 
@@ -126,35 +150,46 @@ static void GobuGizmos_Draw(ecs_iter_t* it)
 {
     ecs_gizmos_t* gizmos = ecs_field(it, ecs_gizmos_t, 1);
     GBoundingBox* box = ecs_field(it, GBoundingBox, 2);
+    GPosition* post = ecs_field(it, GPosition, 3);
+    GScale* sca = ecs_field(it, GScale, 4);
+    GRotation* rota = ecs_field(it, GRotation, 5);
 
     for (int i = 0; i < it->count; i++)
     {
         if (!gizmos[i].selected) continue;
-
         Rectangle bonding = (Rectangle){ box[i].min.x, box[i].min.y, box[i].max.x, box[i].max.y };
 
-        DrawRectangleLinesEx(bonding, 2, SKYBLUE);
+        rlPushMatrix();
+        {
+            rlTranslatef(bonding.x, bonding.y, 0.0f);
+            rlRotatef(rota[i].x, 0.0f, 0.0f, 1.0f);
+            rlRotatef(rota[i].y, 1.0f, 0.0f, 0.0f);
+            rlTranslatef(-bonding.x, -bonding.y, 0.0f);
 
-        // creamos 4 rectangulos en cada esquina del bounding box para cambiar el tamaño de la entidad
-        Rectangle rect1 = (Rectangle){ bonding.x - 4, bonding.y - 4, 8, 8 };
-        Rectangle rect2 = (Rectangle){ bonding.x - 4, bonding.y + bonding.height - 4, 8, 8 };
-        Rectangle rect3 = (Rectangle){ bonding.x + bonding.width - 4, bonding.y - 4, 8, 8 };
-        Rectangle rect4 = (Rectangle){ bonding.x + bonding.width - 4, bonding.y + bonding.height - 4, 8, 8 };
+            DrawRectangleLinesEx(bonding, 2, SKYBLUE);
 
-        DrawRectangleRec(rect1, SKYBLUE);
-        DrawRectangleRec(rect2, SKYBLUE);
-        DrawRectangleRec(rect3, SKYBLUE);
-        DrawRectangleRec(rect4, SKYBLUE);
+            // creamos 4 rectangulos en cada esquina del bounding box para cambiar el tamaño de la entidad
+            Rectangle rect1 = (Rectangle){ bonding.x - 4, bonding.y - 4, 8, 8 };
+            Rectangle rect2 = (Rectangle){ bonding.x - 4, bonding.y + bonding.height - 4, 8, 8 };
+            Rectangle rect3 = (Rectangle){ bonding.x + bonding.width - 4, bonding.y - 4, 8, 8 };
+            Rectangle rect4 = (Rectangle){ bonding.x + bonding.width - 4, bonding.y + bonding.height - 4, 8, 8 };
 
-        // En cada rectangulo dibujamos un cuadrado de color blanco para poder verlo
-        DrawRectangle(rect1.x + 2.5, rect1.y + 2.5, 4, 4, WHITE);
-        DrawRectangle(rect2.x + 2.5, rect2.y + 2.5, 4, 4, WHITE);
-        DrawRectangle(rect3.x + 2.5, rect3.y + 2.5, 4, 4, WHITE);
-        DrawRectangle(rect4.x + 2.5, rect4.y + 2.5, 4, 4, WHITE);
+            DrawRectangleRec(rect1, SKYBLUE);
+            DrawRectangleRec(rect2, SKYBLUE);
+            DrawRectangleRec(rect3, SKYBLUE);
+            DrawRectangleRec(rect4, SKYBLUE);
 
-        // Creamos un círculo de línea en el centro del bounding box para rotar la entidad seleccionada
-        Vector2 center = (Vector2){ bonding.x + bonding.width / 2, (bonding.y + bonding.height / 2) + (bonding.height / 2 + 20) };
-        DrawCircle(center.x, center.y, 5, WHITE);
-        DrawCircleLines(center.x, center.y, 5, BLACK);
+            // En cada rectangulo dibujamos un cuadrado de color blanco para poder verlo
+            DrawRectangle(rect1.x + 2.5, rect1.y + 2.5, 4, 4, WHITE);
+            DrawRectangle(rect2.x + 2.5, rect2.y + 2.5, 4, 4, WHITE);
+            DrawRectangle(rect3.x + 2.5, rect3.y + 2.5, 4, 4, WHITE);
+            DrawRectangle(rect4.x + 2.5, rect4.y + 2.5, 4, 4, WHITE);
+
+            // Creamos un círculo de línea en el centro del bounding box para rotar la entidad seleccionada
+            Vector2 center = (Vector2){ bonding.x + bonding.width / 2, (bonding.y + bonding.height / 2) + (bonding.height / 2 + 20) };
+            DrawCircle(center.x, center.y, 5, WHITE);
+            DrawCircleLines(center.x, center.y, 5, BLACK);
+        }
+        rlPopMatrix();
     }
 }
